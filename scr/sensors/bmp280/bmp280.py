e08053ff9927176c2e77e7d154769942a9ef2423
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-19 00:17:06 +0100

    .

--- scr/sensors/bmp280/bmp280.py
+++ scr/sensors/bmp280/bmp280.py
@@ -4,7 +4,8 @@ import adafruit_bmp280
 
 def read_data():
     i2c = busio.I2C(board.SCL, board.SDA)
-    sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+    # Specify the correct I2C address 
+    sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=0x76)
 
     # Set the sea level pressure (hPa) at your location for accurate altitude readings
     sensor.sea_level_pressure = 1013.25
