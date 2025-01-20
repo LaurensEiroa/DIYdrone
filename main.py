73ad54695eb8e387c23042e4ec17f9618a14eaea
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-20 21:11:22 +0100

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from src.websocket.websocket_client import run
-from src.Engine.pwm import test_motor
+from src.Engine.pwm import test as test_motor
 from src.sensors.mpu6050.mpu6050 import test as test_mpu
 
 if __name__=="__main__":
