bc413c68d7ea891ce94957f6b676afe7a1af37bb
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-21 13:54:32 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import asyncio
-from src.websocket.websocket_client import run
+from src.coms.websocket.websocket_client import run
 from src.Engine.pwm import test as test_motor
-from src.sensors.mpu6050.mpu6050 import test as test_mpu
+from src.sensors.mpu6050.mpu6050 import read_rata as read_data_mpu
+from src.picamera.picamera import Camera
+from src.coms.udp.udp_sender import UDPSender
+from config import Config
+
+
+def data_to_string(data):
+    return "//".join(data)
+
+
+async def run():
+    camera = Camera()
+    
+    sender = 'piZero4'  # Replace with your sender IP address
+    receiver = 'ubuntu_laptop'  # Replace with your receiver IP address
+
+    udp_data_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+    udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+
+    while True:
+        frame = camera.get_frame()
+        a,g,t = read_data_mpu()
+        data = data_to_string(g)
+        await asyncio.gather(
+            udp_frame_sender.send_data(frame,data_type="frame"),
+            udp_data_sender.send_data(data,data_type="data")
+        )
 
 if __name__=="__main__":
+    asyncio.get_event_loop().run_until_complete(run())
 
-    #run()
-    #print("starting test")
-    test_mpu()
+
---
+++ src/coms/udp/udp_sender.py
@@ -0,0 +1,50 @@
+import socket
+import cv2
+from config import Config
+import asyncio
+
+
+
+class UDPSender:
+    def __init__(self, sender_ip, receiver_ip, port):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.server_socket.bind((sender_ip, port))
+        self.receiver_address = (receiver_ip, port)
+
+    async def send_data(self,data,data_type="image"):
+        if data_type=="frame": 
+            encoded, buffer = cv2.imencode('.jpg', data)
+            buffer = buffer.tobytes()
+            MAX_DGRAM = Config.MAX_DGRAM_FRAME
+        elif data_type=="data":
+            buffer = data.encode('utf-8')
+            MAX_DGRAM = Config.MAX_DGRAM_DATA        
+        size = len(buffer)
+        for i in range(0, size, MAX_DGRAM):
+            self.server_socket.sendto(buffer[i:i+MAX_DGRAM], self.receiver_address)
+
+async def run():
+    # Capture video from the default camera
+    sender = 'piZero4'  # Replace with your sender IP address
+    receiver = 'ubuntu_laptop'  # Replace with your receiver IP address
+
+    udp_data_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+    udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+
+    cap = cv2.VideoCapture(0)
+    data = "Hello from the drone!"
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        data = data[::-1]
+        await asyncio.gather(
+            udp_frame_sender.send_data(frame,data_type="frame"),
+            udp_data_sender.send_data(data,data_type="data")
+        )
+    cap.release()
+
+if __name__=="__main__":
+    asyncio.get_event_loop().run_until_complete(run())
