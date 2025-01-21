95cf01a876a7d4870f85c387fe8efb53edaa13e4
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-21 15:11:49 +0100

    .

--- src/picamera/picamera.py
+++ src/picamera/picamera.py
@@ -5,7 +5,7 @@ class Camera:
     def __init__(self,resolution=(340, 280), format='XRGB8888'):
         # Initialize Picamera2
         self.picam2 = Picamera2()
-        self.picam2.configure(self.camera.create_video_configuration(main={"format": format, "size": resolution}))
+        self.picam2.configure(self.picam2.create_video_configuration(main={"format": format, "size": resolution}))
         self.picam2.start()
 
     def get_frame(self):
