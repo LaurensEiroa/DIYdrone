b9c5f2db363eed34166ac29191b9ed38fea8339a
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-21 16:10:52 +0100

    .

--- src/coms/udp/udp_sender.py
+++ src/coms/udp/udp_sender.py
@@ -3,8 +3,6 @@ import cv2
 from config import Config
 import asyncio
 
-
-
 class UDPSender:
     def __init__(self, sender_ip, receiver_ip, port):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,26 +11,27 @@ class UDPSender:
         self.server_socket.bind((sender_ip, port))
         self.receiver_address = (receiver_ip, port)
 
-    async def send_data(self,data,data_type="image"):
+    async def send_data(self, data, data_type="image"):
         print(f"sending {data_type}")
-        if data_type=="frame": 
+        if data_type == "frame":
             encoded, buffer = cv2.imencode('.jpg', data)
             buffer = buffer.tobytes()
             MAX_DGRAM = Config.MAX_DGRAM_FRAME
-        elif data_type=="data":
+        elif data_type == "data":
             buffer = data.encode('utf-8')
-            MAX_DGRAM = Config.MAX_DGRAM_DATA        
+            MAX_DGRAM = Config.MAX_DGRAM_DATA
         size = len(buffer)
         for i in range(0, size, MAX_DGRAM):
             self.server_socket.sendto(buffer[i:i+MAX_DGRAM], self.receiver_address)
+        # Send a delimiter to indicate the end of the frame
+        self.server_socket.sendto(b'END', self.receiver_address)
 
 async def run():
-    # Capture video from the default camera
     sender = 'piZero4'  # Replace with your sender IP address
     receiver = 'ubuntu_laptop'  # Replace with your receiver IP address
 
     udp_data_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
-    #udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+    udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_FRAME_PORT)
 
     cap = cv2.VideoCapture(0)
     data = "Hello from the drone!"
@@ -42,10 +41,10 @@ async def run():
             break
         data = data[::-1]
         await asyncio.gather(
-            #udp_frame_sender.send_data(frame,data_type="frame"),
-            udp_data_sender.send_data(data,data_type="data")
+            udp_frame_sender.send_data(frame, data_type="frame"),
+            udp_data_sender.send_data(data, data_type="data")
         )
     cap.release()
 
-if __name__=="__main__":
-    asyncio.get_event_loop().run_until_complete(run())
+if __name__ == "__main__":
+    asyncio.run(run())
