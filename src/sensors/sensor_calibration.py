1ef06fc72cef650acfe37057025896fc8f989290
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-24 16:14:19 +0100

    .

--- src/sensors/sensor_calibration.py
+++ src/sensors/sensor_calibration.py
@@ -17,11 +17,7 @@ def read_samples(samples=1000):
     for _ in _samples:
         t0 = time.time()
         acceleration.append(mpu.acceleration)
-        delta_t = time.time()-t0
-        if delta_t>=1/1000:
-            continue
-        else:
-            time.sleep(hz-delta_t)
+        time.sleep(hz-time.time()-t0)
     
     print(f"end: {time.time()}")
     np.savetxt("data/accelerometer_data.txt",np.asanyarray(acceleration))
