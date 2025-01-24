911ed724d33bcbff2fa83a715064cf83a779e516
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-24 22:42:35 +0100

    .

--- src/sensors/sensor_calibration.py
+++ src/sensors/sensor_calibration.py
@@ -11,14 +11,14 @@ def read_samples(samples=1000):
     i2c = busio.I2C(board.SCL, board.SDA)
     mpu = adafruit_mpu6050.MPU6050(i2c)
     _samples = range(samples)
-    acceleration = np.zeros((samples,3))
+    acceleration = []
     hz = 1/samples
     t0 = time.time()
     for i in _samples:
-        acceleration[i,:] = np.asarray(mpu.acceleration)
+        acceleration.append(mpu.acceleration)
     tf = time.time()
     print(f"total time: {tf-t0}, frequency: {(tf-t0)/samples}")
-    np.savetxt("data/accelerometer_data.txt",acceleration)
+    np.savetxt("data/accelerometer_data.txt",np.asarrayS(acceleration))
 
 if __name__=="__main__":
     read_samples()
