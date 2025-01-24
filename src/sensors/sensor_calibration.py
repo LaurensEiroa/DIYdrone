cf83f76f4a9dea7cfd1f9fb9b1e18bfe5625e2bf
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-24 23:24:52 +0100

    .

--- src/sensors/sensor_calibration.py
+++ src/sensors/sensor_calibration.py
@@ -4,6 +4,7 @@ import board
 import busio
 import adafruit_mpu6050
 import numpy as np
+from scipy.signal import butter, filtfilt
 
 
 
@@ -20,5 +21,35 @@ def read_samples(samples=1000):
     print(f"total time: {tf-t0}, frequency: {(tf-t0)/samples}")
     np.savetxt("data/accelerometer_data.txt",np.asarray(acceleration))
 
+def process_data():
+    import matplotlib.pyplot as plt
+    path= "/home/laurens/Descargas/accelerometer_data.txt"
+    data = np.loadtxt(path)
+    samples = 1000
+    sample_rate = 0.001933177947998047
+
+    t = np.linspace(0,samples*sample_rate,samples,endpoint=False)
+    print(f"time: {t[1]-t[0]}")
+    plt.figure("data plot")
+    plt.plot(t,data)
+
+
+    yf = np.fft.fft(data[:,0])
+    xf = np.fft.fftfreq(samples, sample_rate)[:samples//2]
+
+
+    plt.figure("Frequency Domain Signal a_x")
+    plt.plot(xf, 2.0/samples * np.abs(yf[:samples//2]))
+    plt.grid()
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Amplitude")
+
+
+
+
+    plt.show()
+
+
 if __name__=="__main__":
-    read_samples()
+    #read_samples()
+    process_data()
