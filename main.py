c444b14e52ea7c974d8964d9ef0127257fda63cf
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-25 17:07:55 +0100

    .

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ async def run():
     receiver = Config.RECIEVER  # Replace with your receiver IP address
 
     udp_data_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT, obj=drone)
-    udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_FRAME_PORT, obj=drone)
+    #udp_frame_sender = UDPSender(sender_ip=Config.IPs[sender], receiver_ip=Config.IPs[receiver], port=Config.UDP_FRAME_PORT, obj=drone)
 
     
     await asyncio.gather(
             drone.start_drone(),
             udp_data_sender.run_udp("data"),
-            udp_frame_sender.run_udp("frame"),
+            #udp_frame_sender.run_udp("frame"),
         )
 
 if __name__=="__main__":
