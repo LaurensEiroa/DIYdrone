a80a911eebf34900e3e31597ddb48e77ef8117bf
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-25 17:40:37 +0100

    .

--- src/Drone.py
+++ src/Drone.py
@@ -9,7 +9,7 @@ from src.sensors.bmp280.bmp280 import read_data as read_bmp
 import asyncio
 
 class Drone:
-    def __init__(self,length_width_height = [90,60,20],window_size=1,threshold=0.0):
+    def __init__(self,length_width_height = [90,60,20],window_size=5,threshold=0.2):
         self.threshold = threshold
         self.length_width_height = np.asarray(length_width_height)
 
